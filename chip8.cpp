ae055dd6db76a0323e780f4f35b574bacf275b49
jczy3 <yaboyjc@yahoo.com>
2026-02-25 19:38:18 -0800

    Implemented first 10 opcodes in chip8.cpp

--- chip8.cpp
+++ chip8.cpp
@@ -1,4 +1,10 @@
 #include "chip8.h"
+#include <iostream>
+using std::cout;
+#define VX V[opcode & 0x0F00] 
+#define VY V[opcode & 0x00F0] 
+#define NN opcode & 0x00FF
+#define NNN opcode & 0x0FFF
 
 chip8::chip8()
 {
@@ -25,6 +31,94 @@ void chip8::emulateCycle()
   opcode = memory[pc] << 8 | memory[pc + 1];
 
   // Decode/Execute Opcode
+  switch (opcode & 0xF000)
+  {
+    case 0x0000:
+      switch(opcode & 0x000F)
+      {
+        case 0x00E0:    // Clear the screen
+
+        break;
+
+        case 0x00EE:    // Return from subroutine
+          pc = stack[sp];
+          --sp;
+        break;
+
+        default:
+          cout << "Unknown opcode [0x0000]: 0x" << opcode << "\n";
+      }
+    break;
+
+    case 0x1000:        // Jump to address NNN
+      pc = NNN;
+    break;
+      
+    case 0x2000:        // Call subroutine at NNN
+      stack[sp] = pc;
+      ++sp;
+      pc = NNN;
+    break;
+
+    case 0x3000:        // (3XNN) Skips next instruction if VX == NN
+      if (VX == NN)
+        pc += 4;
+    break;
+
+    case 0x4000:        // (4XNN) Skips next instruction if VX != NN
+      if (VX != NN)
+        pc += 4;
+    break;
+
+    case 0x5000:        // (5XY0) Skips next instruction if VX == VY
+      if (VX == VY)
+        pc += 4;
+    break;
+
+    case 0x6000:        // (6XNN) Sets VX = NN
+      VX = NN;
+    break;
+
+    case 0x7000:        // (7XNN) Adds NN to VX (carry flag unchanged)
+      VX += NN;
+    break;
+
+    case 0x8000:
+      switch (opcode & 0x000F){
+        case 0x0001:
+
+        break;
+
+        case 0x0002:
+
+        break;
+
+        case 0x0003:
+
+        break;
+
+        case 0x0004:
+
+        break;
+
+        case 0x0005:
+
+        break;
+
+        case 0x0006:
+
+        break;
+
+        case 0x0007:
+
+        break;
+
+        case 0x000E:
+
+        break;
+      }
+    break;
+  }
 
   // Update Timers
   if (delay_timer > 0)
